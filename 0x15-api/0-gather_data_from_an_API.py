2e63b81edc31f8214919aa787cdeb8550cb1cd47
Bryan Somto <bryansomto@gmail.com>
2022-10-18 03:21:29 +0100

    fixed pep8 requirement fail

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,11 +10,10 @@ import sys
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/"
     response = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(api_url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = requests.get(
+        api_url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         response.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
-
-    
