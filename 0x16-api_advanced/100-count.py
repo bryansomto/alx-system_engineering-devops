43a2d8ffb8f437bfafc4e52320709be278f84dd7
Bryan Somto <bryansomto@gmail.com>
2022-11-02 03:42:29 +0100

    extra modifs

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,7 +6,8 @@ import requests
 
 
 def count_words(subreddit, word_list, after='', word_dict={}):
-    """ A function that queries the Reddit API parses the title of
+    """
+    A function that queries the Reddit API parses the title of
     all hot articles, and prints a sorted count of given keywords
     (case-insensitive, delimited by spaces.
     Javascript should count as javascript, but java should not).
